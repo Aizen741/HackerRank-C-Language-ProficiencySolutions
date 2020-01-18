6714317a4ca76d81ccec08e8f6a01c1e7b60b89e
Aizen741 <rahulsivadas2009@gmail.com>
2020-01-18 15:42:03 +0530

    Functions

--- Functions/main.c
+++ Functions/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int max_of_four(int a, int b, int c, int d){
+
+    return ((a>b ? a:b) > (c>d ? c:d)) ? (a>b ? a:b) : (c>d ? c:d);
+
+/*The ?: operator works as follows:
+
+condition ? value_if_true : value_if_false
+
+so return (a > b ? a : b);
+could be rewritten as:
+if(a > b)
+    return b;
+else
+    return a;
+*/
+
+}
+
+int main() {
+
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+
     return 0;
 }
+
+
